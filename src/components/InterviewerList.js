2ab5219aaba6f6324f5701850e5c8d13d166d29c
PlumScum <jelanisands@gmail.com>
2023-08-13 18:25:10 -0400

    Add export default function/base HTML

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,3 +2,26 @@ import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
 import PropTypes from 'prop-types';
+
+export default function InterviewerList(props) {
+    const parsedInterviewers = props.interviewers.map((person) => (
+        <InterviewerListItem
+        key={person.id}
+        name={person.name}
+        avatar={person.avatar}
+        selected={person.id === props.value}
+        setInterviewer={() => props.onChange(person.id)}
+        {...person} />
+    ))
+
+    return(
+        <section className="interviewers">
+            <h4 className="interviewers__header text--light">Interviewer</h4>
+            <ul className="interviewers__list">{parsedInterviewers}</ul>
+        </section>
+    )
+    }
+    
+    InterviewerList.propTypes = {
+        parsedInterviewers: PropTypes.array.isRequired
+    };
