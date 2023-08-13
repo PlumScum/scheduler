abfb580d8e90307dea2ad404f9f04cfc58cfe45f
PlumScum <jelanisands@gmail.com>
2023-08-13 15:42:15 -0400

    Add DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,2 +1,23 @@
 import React from "react";
 import DayListItem from "./DayListItem";
+
+const DayList = ({ days, selectedDay, setDay }) => {
+  return (
+    <div>
+      <h2>Select a Day</h2>
+      <ul>
+        {days.map((day, index) => (
+          <li
+            key={index}
+            onClick={() => setDay(day)}
+            className={selectedDay === day ? 'selected' : ''}
+          >
+            {day}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DayList;
