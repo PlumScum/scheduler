4b964e44e44c3e224a299fb1a3ac20efdc44e014
PlumScum <jelanisands@gmail.com>
2023-08-13 17:41:58 -0400

    Incorporate useState into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,30 +21,32 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [selectedDay, setSelectedDay] = useState("Monday"); // Initialize the state with "Monday"
+
   return (
     <main className="layout">
       <section className="sidebar">
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"><DayList
-  days={days}
-  day={"Monday"}
-  setDay={day => console.log(day)}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={selectedDay} // Pass the selectedDay state as a prop
+            setDay={setSelectedDay} // Pass the state update function
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
       </section>
     </main>
   );
