bbe0e590d3338c2c654844a00a41c57a817c452a
PlumScum <jelanisands@gmail.com>
2023-08-13 19:14:06 -0400

    Remove pointless DayList import

--- src/components/__tests__/DayListItem.test.js
+++ src/components/__tests__/DayListItem.test.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-import { render, cleanup} from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 import DayListItem from "components/DayListItem";
 
-import DayList from "components/DayList";
 
 afterEach(cleanup);
 
