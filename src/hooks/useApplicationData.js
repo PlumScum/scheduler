cf1a51318373722d3796f12fab1ccb8ebb3f8c8a
PlumScum <jelanisands@gmail.com>
2023-08-14 05:20:11 -0400

    Add useState and setDay

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,3 +2,15 @@ import React from "react";
 import "components/Application.scss";
 import { useState, useEffect } from "react";
 import axios from "axios";
+
+export default function useApplicationData(props) {
+
+    const [state, setState] = useState({
+      day: "Monday",
+      days: [],
+      appointments:{},
+      interviewers:{}
+    })
+  
+    const setDay = day => setState({ ...state, day });
+};
