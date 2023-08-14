0617ca74b74f06d672af1850368f291344ddff1e
PlumScum <jelanisands@gmail.com>
2023-08-13 22:02:20 -0400

    Refactor InterviewerList components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,27 +1,28 @@
-import React from "react";
-import "components/InterviewerList.scss"
-import InterviewerListItem from "./InterviewerListItem";
+import React from 'react';
+import 'components/InterviewerList.scss';
+import InterviewerListItem from 'components/InterviewerListItem';
 import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
-    const parsedInterviewers = props.interviewers.map((person) => (
-        <InterviewerListItem
-        key={person.id}
-        name={person.name}
-        avatar={person.avatar}
-        selected={person.id === props.value}
-        setInterviewer={() => props.onChange(person.id)}
-        {...person} />
-    ))
+  const parsedInterviewers = props.interviewers.map(interviewer => (
+    <InterviewerListItem
+      key={interviewer.id}
+      selected={interviewer.id === props.value}
+      setInterviewer={() => props.onChange(interviewer.id)}
+      {...interviewer} // Spread the interviewer object
+    />
+  ));
 
-    return(
-        <section className="interviewers">
-            <h4 className="interviewers__header text--light">Interviewer</h4>
-            <ul className="interviewers__list">{parsedInterviewers}</ul>
-        </section>
-    )
-    }
-    
-    InterviewerList.propTypes = {
-        parsedInterviewers: PropTypes.array.isRequired
-    };
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewers}</ul>
+    </section>
+  );
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+};
