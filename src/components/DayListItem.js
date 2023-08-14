636450e417c982174dc769a84730be34ec6e6aab
PlumScum <jelanisands@gmail.com>
2023-08-13 22:14:54 -0400

    Refactor for React

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,26 +1,28 @@
 import React from "react";
 import classNames from "classnames";
-import "components/DayListItem.scss";
+import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
-  const formatSpots = (spots) => {
-    if (spots === 0) {
-      return "no spots remaining";
-    } else if (spots === 1) {
-      return "1 spot remaining";
-    } else {
-      return `${spots} spots remaining`;
-    }
-  };
-
-  const dayClasses = classNames("day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0,
-  });
+    "day-list__item--full": props.spots === 0
+  })
+
+  const formatSpots = () => {
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining"
+    }
+    if (props.spots === 0) {
+      return "no spots remaining"
+    }
+  }
 
   return (
-    <li className={dayClasses} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
+    <li data-testid="day" className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
