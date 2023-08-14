13a1f0d30b490b878dcb8f2bb8caf512dd0ab742
PlumScum <jelanisands@gmail.com>
2023-08-13 23:14:09 -0400

    Initial commit, add base HTML function

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointment = () => {
+  return (
+    <article className="appointment">
+      {/* Your content here */}
+    </article>
+  );
+};
+
+export default Appointment;
